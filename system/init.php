9d90ad994c1aeea0e568c2eb90763afc17371026
Fauzan Falah <fauzanfalah21@gmail.com>
2020-04-30 14:03:42 +0700

    update init

--- system/init.php
+++ system/init.php
@@ -2,17 +2,18 @@
     require_once 'app/Config/Config.php';
     require_once 'app/Config/Debug.php';
     require_once 'app/Config/Autoload.php';
+    
+    require_once 'app/Config/Database.php';
+    require_once 'Database.php';
     require_once 'Function.php';
     require_once 'Session.php';
     require_once 'Input.php';
-    
+
     require_once 'Controller.php';
     require_once 'App.php';
     require_once 'Views.php';
     require_once 'Crud.php';
 
-    require_once 'app/Config/Database.php';
-    require_once 'Database.php';
 
 
     $app = new App(default_controller);
